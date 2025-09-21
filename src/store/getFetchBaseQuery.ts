3b0551f2bcc9f703a3b6af293c404851856b45b9
esara_ahmed <esara_ahmed@outlook.com>
2025-09-21 17:08:03 +0300

    install new backend domain

--- src/store/getFetchBaseQuery.ts
+++ src/store/getFetchBaseQuery.ts
@@ -1,8 +1,8 @@
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 import { AppState } from '@/src/store/index';
 
-let baseUrl = (process.env.BASE_URL || 'https://darb-be.v4.mmd-technology.com') + '/api/';
-
+//let baseUrl = (process.env.BASE_URL || 'https://darb-be.v4.mmd-technology.com') + '/api/';
+let baseUrl = (process.env.BASE_URL || 'https://darb0org-001-site1.mtempurl.com') + '/api/';
 
 export const getFetchBaseQuery = fetchBaseQuery({
 	baseUrl,
